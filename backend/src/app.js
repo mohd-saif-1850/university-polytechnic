f570614fee7977cc4696d7c34b773b3b2dbcda1a
Mohd Saif <iamg6102@gmail.com>
2025-09-18 23:52:29 +0530

    vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,24 +5,26 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
-app.use(cors({
-  origin: process.env.CORS_ORIGIN || "http://localhost:5173",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    credentials: true,
+  })
+);
 
+// Root route
 app.get("/", (req, res) => {
   res.send("🚀 Polytechnic Store Backend is running!");
 });
 
 // Routes
 import itemRoute from "./routes/item.route.js";
-import formRoute from "./routes/form.route.js"
-import consumedRoute from "./routes/consumed.route.js"
+import formRoute from "./routes/form.route.js";
+import consumedRoute from "./routes/consumed.route.js";
 
 // Base path
 app.use("/api/v1/items", itemRoute);
-app.use("/api/v1/forms",formRoute)
-app.use("/api/v1",consumedRoute)
-
+app.use("/api/v1/forms", formRoute);
+app.use("/api/v1", consumedRoute);
 
 export default app;
