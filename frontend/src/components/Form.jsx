557d98e63ba2b5ebf15e11ac2d038729e5b68db2
Mohd Saif <iamg6102@gmail.com>
2025-09-19 00:40:42 +0530

    dekhl re

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -14,7 +14,8 @@ function Form() {
   const [success, setSuccess] = useState("");
   const [itemsList, setItemsList] = useState([]);
 
-  const API_URL = "http://localhost:5000/api/v1";
+  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1";
+
 
   const fetchItems = async () => {
     try {
