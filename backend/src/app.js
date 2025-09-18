e00e91bf13d1ef9c5c044ff819e3ad51b61b0693
Mohd Saif <iamg6102@gmail.com>
2025-09-19 00:23:21 +0530

    app

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "*",
+    origin: [process.env.CORS_ORIGIN,"https://university-polytechnic-w6k1.vercel.app"],
     credentials: true,
   })
 );
