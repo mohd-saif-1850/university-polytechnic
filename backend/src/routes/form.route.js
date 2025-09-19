e46991821ef7fff81ff4b7c1fe6f07ae8800900c
Mohd Saif <iamg6102@gmail.com>
2025-09-20 01:02:49 +0530

    backedn fixing try

--- backend/src/routes/form.route.js
+++ backend/src/routes/form.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createForm, deleteForm, updateForm } from "../controllers/form.controller.js";
+import { createForm, deleteForm, getAllForms, updateForm } from "../controllers/form.controller.js";
 
-const router = Router()
+const router = Router();
 
-router.route("/add-form").post(createForm)
-router.route("/update-form").patch(updateForm)
-router.route("/delete-form").delete(deleteForm)
+router.route("/add-form").post(createForm);
+router.route("/update-form").patch(updateForm);
+router.route("/delete-form").delete(deleteForm);
+router.route("/get-forms").get(getAllForms);
 
 export default router;
